4d14502055335ee5b3bc6ce0f457a18401a17e7e
Jacob Cram <cramjaco@gmail.com>
2019-06-06 13:56:15 -0400

    Convert figure files to tiff

---
+++ scripts/processfigures.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# modified from from https://gist.github.com/matsen/4263955
+cd ../figures
+# figure 1
+inkscape --without-gui --export-png="useiggs.png" -- export-dpi 300 useiggs.svg
+convert -compress LZW -alpha remove useiggs.png useiggs.tiff
+mogrify -alpha off useiggs.tiff
+mv useiggs.tiff numbered/figure1.tiff
+rm useiggs.png
+
+# figure 2
+convert -compress LZW -alpha remove wunifrac_Agp41_Bgp120_pcoa.png numbered/figure2.tiff
+
+# figure 3
+
+convert -compress LZW -alpha remove stacked_bars.png numbered/figure3.tiff
+
+# figure 4
+
+convert -compress LZW -alpha remove phi_heatmap_withlegend.png numbered/figure4.tiff
+
+cd ../scripts
+
